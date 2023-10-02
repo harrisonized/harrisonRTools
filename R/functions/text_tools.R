6ec9ae792723d1757b94a9a39e1bece27c1e42f6
Harrison Wang <harrisonized@gmail.com>
2023-10-02 16:38:55 -0400

    update script to take excel directly without converting to csv

--- R/functions/text_tools.R
+++ R/functions/text_tools.R
@@ -2,10 +2,24 @@
 ## dotsep_to_snake_case
 
 #' Converts "Column.Title" to column_title
+#'
+#' @export
 dotsep_to_snake_case <- function(text) {
     return(tolower(
         paste(
             unlist(strsplit(text, '[.]')), collapse='_')
+        )
+    )
+}
+
+
+#' Converts "Column Title" to column_title
+#'
+#' @export
+title_to_snake_case <- function(text) {
+    return(tolower(
+        paste(
+            unlist(strsplit(text, '[ ]')), collapse='_')
         )
     )
 }
