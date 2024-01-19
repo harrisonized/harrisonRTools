ca32467513ea7ff16d7844aafb82ff90ecf5e275
Harrison Wang <harrisonized@gmail.com>
2024-01-19 17:23:20 -0500

    add mvp for plot_gel

--- R/plotting.R
+++ R/plotting.R
@@ -8,6 +8,7 @@ import::here(magrittr, '%>%')
 ## plot_heatmap
 ## plot_single_line
 ## plot_multiscatter
+## plot_gel
 
 
 #' Plotly's default colors
@@ -308,6 +309,119 @@ plot_multiscatter <- function(
         ),
         plot_bgcolor = 'rgba(0,0,0,0)',
         showlegend = TRUE,
+        hovermode = 'closest'
+    )
+
+    return(fig)
+}
+
+
+#' Plot Gel
+#' 
+#' @description
+#' Insert a dataframe like this one:
+#' +-----------+------+-------------+-----------+------------+-----------+
+#' | gene_name | lane | exon_length | num_reads |    srpm    | intensity |
+#' +-----------+------+-------------+-----------+------------+-----------+
+#' | Tmsb4x    | mat  |     1371    |   25571   |  2878.255  |  1.000000 |
+#' | Arhgap4   | mat  |     8105    |    4681   |   526.889  |  0.346293 |
+#' | Xist      | mat  |    17946    |      58   |     6.476  |  0.201613 |
+#' | Tmsb4x    | pat  |     1370    |      67   |     7.541  |  0.201909 |
+#' | Arhgap4   | pat  |     8105    |      22   |     2.476  |  0.200501 |
+#' | Xist      | pat  |    16791    |    21964  |  2472.253  |  0.887127 |
+#' +-----------+------+-------------+-----------+------------+-----------+
+#' 
+#' @export
+plot_gel <- function(
+    df,
+    lane='lane',
+    genes='gene_name',
+    size='size',
+    intensity_col='intensity',
+    text_col='intensity',
+    column_order=c(),
+    ylabel=NULL,
+    title=NULL,
+    yrange=c(NA, NA),
+    showlegend=FALSE,
+    font_size=12,
+    tickangle=-45,
+    band_width=0.04,
+    lane_width=100,
+    top_margin=150,
+    height=800,
+    min_width=200
+) {
+
+    num_lanes = nrow(unique(df[lane]))
+    yrange = log(yrange, base=10)
+
+    fig <- plot_ly(
+        height =  height,
+        width = min_width+lane_width*num_lanes,
+        type = 'bar'
+    )
+    for (gene in unique(df[[genes]])) {
+        subset = df[(df[genes] == gene), ]
+        fig <- fig %>% add_trace(
+            base=subset[[size]],
+            x=subset[[lane]],
+            y=subset[[size]]*band_width,
+            text=subset[[text_col]],
+            legendgroup=gene,
+            alignmentgroup=TRUE,
+            marker=list(
+                color= c(paste0('rgba(0, 0, 0, ', subset[[intensity_col]]))
+            ),
+            name=gene,
+            orientation='v',
+            showlegend=TRUE,
+            textposition="none",
+            hovertemplate=paste0(
+                'lane=%{x}<br>',
+                'genes=', gene, '<br>',
+                'size=%{base}<br>',
+                text_col, '=%{text}<br>',
+                '<extra></extra>'
+            ),
+            type='bar'
+        )
+    }
+
+    fig <- fig %>% layout(
+        title = list(
+            text = title,
+            x = 0
+        ),
+        xaxis = list(
+            side = 'top',
+            tickangle = tickangle,
+            type = 'category',
+            # autorange = 'reversed',
+            categoryorder = 'array',
+            categoryarray = column_order
+        ),
+        yaxis = list(
+            type = 'log',
+            range = yrange,
+            ticks = 'outside',
+            showline = TRUE,
+            title_text = ylabel
+        ),
+        legend = list(
+            title = list(text = 'genes'),
+            tracegroupgap = 0
+        ),
+        margin = list(
+            t = top_margin
+        ),
+        barmode = 'overlay',
+        autosize = FALSE,
+        plot_bgcolor = 'rgba(0,0,0,0)',
+        showlegend =showlegend,
+        font = list(
+            size = font_size
+        ),
         hovermode = 'closest'
     )
 
